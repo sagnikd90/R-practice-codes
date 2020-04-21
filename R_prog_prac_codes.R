1ae53018916a004686688722bcaaec6862264ab0
Sagnik Das <das.90.sagnik@gmail.com>
2020-04-21 02:59:24 -0400

    Will be my practice codes

---
+++ R_prog_prac_codes.R
@@ -0,0 +1,24 @@
+setwd("C:/Users/das90/OneDrive/Coursera courses/John Hopkins/R Codes and Files")
+
+#####R programming practice#######
+
+
+
+x<- 1:25
+x
+
+######As function######
+
+x<- 0:20
+as.numeric(x)
+as.character(x)
+as.logical(x)
+
+#########List##########
+
+#####List is a special type of vector which can contain variables of different classes. For example, a list can contain both numeric and character values########
+
+##########Creating a list################
+
+y<- list(1:10, "a", TRUE, 4 +5i)
+print(y)
